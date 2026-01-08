67319246a133905861d4c9d8dbd44b6bb053c9ef
Paweł Zatoka <github@hoxi.me>
2026-01-09 00:52:44 +0100

    Don't reinstall Rosetta if it's already installed

--- home/helpers/install-rosetta.nix
+++ home/helpers/install-rosetta.nix
@@ -1,5 +1,15 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "install-rosetta" ''
   set -e
-  /usr/sbin/softwareupdate --install-rosetta
+  if /usr/bin/pgrep -q oahd; then
+    echo "Rosetta is already installed"
+    exit 0
+  fi
+
+  if arch -x86_64 /usr/bin/true 2>/dev/null; then
+    echo "Rosetta is already installed"
+    exit 0
+  fi
+
+  /usr/sbin/softwareupdate --install-rosetta --agree-to-license
 ''
