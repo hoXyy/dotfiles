225fe08232c563d9bdaedc7c66d709813ea5486a
Paweł Zatoka <github@hoxi.me>
2026-01-11 02:45:27 +0100

    Fix nix-rebuild alias

--- home/zsh.nix
+++ home/zsh.nix
@@ -81,7 +81,8 @@
     shellAliases = {
       ls = "ls -l --color";
       lg = "lazygit";
-      nix-rebuild = "sudo darwin-rebuild switch --flake ''\"git+file://$HOME/.config/nix?submodules=1#hoxi-mbp''\"";
+      nix-rebuild = ''
+        sudo darwin-rebuild switch --flake "git+file://$HOME/.config/nix?submodules=1#hoxi-mbp"'';
     };
   };
 }
