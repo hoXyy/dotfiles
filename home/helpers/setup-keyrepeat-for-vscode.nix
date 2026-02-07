57c735e7c5e0f77a651c9a8444c4b8600ecdbc6a
Paweł Zatoka <github@hoxi.me>
2026-02-07 02:23:02 +0100

    Enable the keyrepeat for Cursor as well

--- home/helpers/setup-keyrepeat-for-vscode.nix
+++ home/helpers/setup-keyrepeat-for-vscode.nix
@@ -2,4 +2,5 @@
 pkgs.writeShellScriptBin "setup-keyrepeat-for-vscode" ''
   set -e
   /usr/bin/defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+  /usr/bin/defaults write com.todesktop.230313mzl4w4u92 ApplePressAndHoldEnabled -bool false
 ''
