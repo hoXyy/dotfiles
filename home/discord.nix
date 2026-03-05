e09d93bbab405afb2a93b262f8f4659c0cbc2b24
Paweł Zatoka <github@hoxi.me>
2026-03-05 22:41:37 +0100

    enable openasar for discord

--- home/discord.nix
+++ home/discord.nix
@@ -1,6 +1,9 @@
-{
+{pkgs, ...}: {
   programs.discord = {
     enable = true;
+    package = pkgs.discord.override {
+      withOpenASAR = true;
+    };
     settings = {
       DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
     };
