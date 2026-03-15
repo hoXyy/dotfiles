cc0f9f473ea7014cec018782ca9c07d7638f7331
Paweł Zatoka <github@hoxi.me>
2026-03-15 13:42:05 +0100

    rework ghostty config

--- darwin/packages.nix
+++ darwin/packages.nix
@@ -6,7 +6,6 @@
   environment.systemPackages = with pkgs; [
     git
     neovim
-    ghostty-bin
     rustup
     gh
     bat
--- home/ghostty.nix
+++ home/ghostty.nix
@@ -1,3 +1,19 @@
-{
-  home.file."Library/Application Support/com.mitchellh.ghostty/config".source = ../configs/ghostty;
+{pkgs, ...}: {
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
+    installBatSyntax = true;
+    package = pkgs.ghostty-bin;
+    settings = {
+      macos-option-as-alt = true;
+      font-family = "JetBrainsMono Nerd Font Mono";
+      font-size = 14;
+      font-thicken = true;
+      adjust-cell-height = 2;
+      shell-integration-features = "sudo,ssh-env,no-title";
+      window-padding-x = 8;
+      window-padding-y = 8;
+      theme = "One Double Dark";
+    };
+  };
 }
