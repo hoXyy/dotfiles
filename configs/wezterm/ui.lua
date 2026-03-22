dd742328fe09b915b96fc5f53d040e85a32e37a6
Paweł Zatoka <github@hoxi.me>
2026-03-23 00:51:45 +0100

    clean up wezterm config

--- configs/wezterm/ui.lua
+++ configs/wezterm/ui.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm") ---@type Wezterm
 local colors = {
 	bg = "#1e1e2e",
 	surface0 = "#313244",
-	pink = "#89b4fa", -- The highlight color from your screenshot
+	pink = "#89b4fa",
 	text = "#cdd6f4",
 	subtext0 = "#a6adc8",
 }
@@ -12,7 +12,6 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	local title = tab.active_pane.title
 	local id = tab.tab_index + 1
 
-	-- Add the indicator based on active status
 	local suffix = ""
 	if tab.is_active then
 		suffix = " *"
@@ -20,12 +19,12 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 		suffix = " -"
 	end
 
-	-- Return the string with padding
 	return {
 		{ Text = "  " .. id .. " " .. title .. suffix .. "  " },
 	}
 end)
 
+---@type Config
 return {
 	font_size = 14,
 	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "DemiBold", stretch = "Normal" }),
@@ -40,22 +39,18 @@ return {
 	colors = {
 		tab_bar = {
 			background = colors.bg,
-			-- Inactive tabs
 			inactive_tab = {
 				bg_color = colors.bg,
-				fg_color = colors.pink, -- Pink text for inactive numbers/titles
+				fg_color = colors.pink,
 			},
-			-- Active tab (Solid pink block with dark text)
 			active_tab = {
 				bg_color = colors.pink,
 				fg_color = colors.bg,
 			},
-			-- Hovering over a tab
 			inactive_tab_hover = {
 				bg_color = colors.surface0,
 				fg_color = colors.text,
 			},
-			-- The "new tab" button (+)
 			new_tab = {
 				bg_color = colors.bg,
 				fg_color = colors.text,
--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -8,19 +8,4 @@ for k, v in pairs(ui) do
 	config[k] = v
 end
 
--- local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
--- bar.apply_to_config(config, {
--- 	modules = {
--- 		workspace = {
--- 			enabled = false,
--- 		},
--- 		leader = {
--- 			enabled = false,
--- 		},
--- 		hostname = {
--- 			enabled = false,
--- 		},
--- 	},
--- })
-
 return config
