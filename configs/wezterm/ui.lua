f222ebb3a4713522112a60dbaf0f16ac3fdb89ce
Paweł Zatoka <github@hoxi.me>
2026-03-23 00:30:44 +0100

    wezterm config adjustments
    i dont need plugins for this, neat

--- configs/wezterm/ui.lua
+++ configs/wezterm/ui.lua
@@ -1,13 +1,65 @@
-local wezterm = require("wezterm")
+local wezterm = require("wezterm") ---@type Wezterm
+
+local colors = {
+	bg = "#1e1e2e",
+	surface0 = "#313244",
+	pink = "#89b4fa", -- The highlight color from your screenshot
+	text = "#cdd6f4",
+	subtext0 = "#a6adc8",
+}
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local title = tab.active_pane.title
+	local id = tab.tab_index + 1
+
+	-- Add the indicator based on active status
+	local suffix = ""
+	if tab.is_active then
+		suffix = " *"
+	else
+		suffix = " -"
+	end
+
+	-- Return the string with padding
+	return {
+		{ Text = "  " .. id .. " " .. title .. suffix .. "  " },
+	}
+end)
 
 return {
 	font_size = 14,
 	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "DemiBold", stretch = "Normal" }),
 	tab_bar_at_bottom = true,
 	use_fancy_tab_bar = false,
+	tab_max_width = 32,
 	-- hide_tab_bar_if_only_one_tab = true,
-	color_scheme = "OneDark (base16)",
+	color_scheme = "Catppuccin Mocha",
 	cursor_blink_rate = 500,
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
+	colors = {
+		tab_bar = {
+			background = colors.bg,
+			-- Inactive tabs
+			inactive_tab = {
+				bg_color = colors.bg,
+				fg_color = colors.pink, -- Pink text for inactive numbers/titles
+			},
+			-- Active tab (Solid pink block with dark text)
+			active_tab = {
+				bg_color = colors.pink,
+				fg_color = colors.bg,
+			},
+			-- Hovering over a tab
+			inactive_tab_hover = {
+				bg_color = colors.surface0,
+				fg_color = colors.text,
+			},
+			-- The "new tab" button (+)
+			new_tab = {
+				bg_color = colors.bg,
+				fg_color = colors.text,
+			},
+		},
+	},
 }
--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
-local wezterm = require("wezterm") --@type Wezterm
+local wezterm = require("wezterm") ---@type Wezterm
 local config = wezterm.config_builder()
 local ui = require("ui")
 
@@ -8,19 +8,19 @@ for k, v in pairs(ui) do
 	config[k] = v
 end
 
-local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
-bar.apply_to_config(config, {
-	modules = {
-		workspace = {
-			enabled = false,
-		},
-		leader = {
-			enabled = false,
-		},
-		hostname = {
-			enabled = false,
-		},
-	},
-})
+-- local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+-- bar.apply_to_config(config, {
+-- 	modules = {
+-- 		workspace = {
+-- 			enabled = false,
+-- 		},
+-- 		leader = {
+-- 			enabled = false,
+-- 		},
+-- 		hostname = {
+-- 			enabled = false,
+-- 		},
+-- 	},
+-- })
 
 return config
